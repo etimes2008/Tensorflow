3baf8b33fb3530403f84ac3f68ee69f52f5a22df
吴题 <etimes2001@qq.com>
2017-03-08 15:37:15 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import numpy
+import  math
+
+print("hello world!", math.sin(1.2))
+
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
